73e16b22f4f8fb1113b242e3216ca249aa6e4449
Osama Khader <osama10kh@gmail.com>
2021-05-16 12:12:14 +0300

    exam

---
+++ app.js
@@ -0,0 +1,13 @@
+let flowerImg = document.getElementById('flowerContainer');
+
+let imgArray = ['Alstroemerias','Gardenias','Orchids','Roses','Sunflowers','Tulips','Peonies'];
+
+function Flower(name,image,season){
+    this.name = name;
+    this.image = image;
+    this.season = season;
+}
+
+function render (){
+    
+}
